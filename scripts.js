1b617ca5632450acc7bf7b3381d40a7d93460f71
jesus011282 <creanuevo@hotmail.com>
2025-01-17 23:04:55 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Este es solo un ejemplo para animar el scroll hacia las secciones
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
